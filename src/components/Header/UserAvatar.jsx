a1eaed4d158664f093e85124d1be4c90876c7ce2
周公来 <zhougonglai@nn.com>
2021-03-17 10:26:35 +0800

    avatar 头像问题

--- src/components/Header/UserAvatar.jsx
+++ src/components/Header/UserAvatar.jsx
@@ -7,9 +7,9 @@ function UserAvatar({ userInfo, logout }) {
   const debounceHover = useDebounce(hovering, { wait: 500 });
 
   return (
-    <div className="user-info cursor-pointer text-center flex flex-col items-center justify-center" onMouseEnter={setTrue} onMouseLeave={setFalse}>
-      <img id="avatar" src={userInfo.avatar || avatarX } className="rounded-sm" width="45" alt={userInfo.nickname || userInfo.mobile} />
-      <label htmlFor="avatar">{userInfo.nickname || userInfo.mobile}</label>
+    <div className="user-info cursor-pointer text-center flex items-center justify-center" onMouseEnter={setTrue} onMouseLeave={setFalse}>
+      <img id="avatar" src={userInfo.avatar || avatarX } className="rounded-sm mr-2" width="45" alt={userInfo.nickname || userInfo.mobile} />
+      <label htmlFor="avatar" className="cursor-pointer">{userInfo.nickname || userInfo.mobile}</label>
       {
         debounceHover
         ? <div className="user-menus" onMouseEnter={setTrue}>
