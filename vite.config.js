fb89bc67e52fccdfad1761da0f1de6db80c34b42
周公来 <zhougonglai@nn.com>
2021-03-25 09:29:14 +0800

    PWA

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import legacy from '@vitejs/plugin-legacy'
 import lessToJS from 'less-vars-to-js'
 import 'module-alias/register'
 import handlebars from 'vite-plugin-handlebars';
+import { VitePWA } from 'vite-plugin-pwa'
 
 const themeVariables = lessToJS(
   fs.readFileSync(resolve(__dirname, './src/style/antd.less'), 'utf8')
@@ -19,6 +20,30 @@ export default defineConfig({
     reactRefresh(),
     handlebars({
       partialDirectory: resolve(__dirname, 'src/partials/'),
+    }),
+    VitePWA({
+      manifest: {
+        name: '奇妙加速器',
+        start_url: '/?home=true',
+        icons: [
+          {
+            "src": "icons/manifest-icon-192.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "maskable any"
+          },
+          {
+            "src": "icons/manifest-icon-512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable any"
+          }
+        ],
+        theme_color: '#000000',
+        background_color: '#ffffff',
+        display: 'fullscreen',
+        orientation: 'portrait'
+      },
     }),
     legacy({
       targets: ['defaults', 'ie 11'],
